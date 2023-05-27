d49cc6c3620b7b64823afdb3c2d0890a715d8e72
Rafif Muhammad <52074659+rafifmuhammad@users.noreply.github.com>
2023-05-27 15:45:05 +0000

    API 79%

--- src/handler/chatsHandler.js
+++ src/handler/chatsHandler.js
@@ -78,7 +78,24 @@ const addChatHandler = async (request, h) => {
     date: datePost,
   }).count();
 
-  if (isDateAvailable < 1) {
+  const isTheSameSender = await db.collection('chats').find({
+    $or: [
+      {
+        $and: [
+          { sender_id: senderId },
+          { receiver_id: receiverId },
+        ],
+      },
+      {
+        $and: [
+          { sender_id: receiverId },
+          { receiver_id: senderId },
+        ],
+      },
+    ],
+  }).count();
+
+  if (isDateAvailable < 1 || isTheSameSender < 1) {
     const newChat = {
       chat_id: new ObjectID(),
       sender_id: senderId,
@@ -101,112 +118,93 @@ const addChatHandler = async (request, h) => {
 
     const messages = await db.collection('chats').updateOne(
       {
+        $or: [
+          {
+            $and: [
+              { sender_id: senderId },
+              { receiver_id: receiverId },
+            ],
+          },
+          {
+            $and: [
+              { sender_id: receiverId },
+              { receiver_id: senderId },
+            ],
+          },
+        ],
         date: datePost,
       },
       { $push: { messages: newMessages } },
     );
 
     console.log(messages);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Pesan terkirim',
+    });
+
+    return response;
   }
 
-  if (isDateAvailable > 0) {
+  if (isDateAvailable > 0 && isTheSameSender > 0) {
     const newMessages = {
       user_id: senderUserId, message_id: new ObjectID(), text, created_at: date,
     };
 
     const messages = await db.collection('chats').updateOne(
       {
-        date: datePost,
+        $or: [
+          {
+            $and: [
+              { sender_id: senderId },
+              { receiver_id: receiverId },
+              { date: datePost },
+            ],
+          },
+          {
+            $and: [
+              { sender_id: receiverId },
+              { receiver_id: senderId },
+              { date: datePost },
+            ],
+          },
+        ],
       },
       { $push: { messages: newMessages } },
     );
 
     console.log(messages);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Pesan terkirim',
+    });
+
+    return response;
   }
 
-  return isDateAvailable;
-  // const isDateAvailable = chats.filter(
-  //   (chat) => chat.date.substring(0, 10) === date.substring(0, 10),
-  // ).length > 0;
-  // const isTheSameSender = chats.filter((chat) => chat.sender_id === senderId
-  // || chat.sender_id === receiverId).length > 0;
+  const response = h.response({
+    status: 'fail',
+    message: 'Pesan tidak terkirim',
+  });
 
-  // if (!isDateAvailable || !isTheSameSender) {
-  //   const newChat = {
-  //     chat_id: chatId,
-  //     sender_id: senderId,
-  //     receiver_id: receiverId,
-  //     name,
-  //     date,
-  //     sender_user_id: senderUserId,
-  //     receiver_user_id: receiverUserId,
-  //     messages: [],
-  //   };
-
-  //   chats.push(newChat);
-
-  //   const isSuccess = chats.filter((chat) => chat.chat_id === chatId).length > 0;
-  //   const index = chats.findIndex(
-  //     (chat) => chat.date.substring(0, 10) === date.substring(0, 10)
-  //     && (chat.sender_id === senderId || chat.sender_id === receiverId),
-  //   );
-
-  //   if (isSuccess) {
-  //     const messageId = nanoid(16);
-  //     const createdAt = new Date().toISOString();
-  //     const newMessage = {
-  //       sender_user_id: senderUserId, message_id: messageId, text, created_at: createdAt,
-  //     };
-
-  //     chats[index].messages.push(newMessage);
-
-  //     const response = h.response({
-  //       status: 'success',
-  //       message: 'pesan berhasil dikirimkan',
-  //     });
-
-  //     response.code(201);
-  //     return response;
-  //   }
-  // }
-
-  // if (isDateAvailable && isTheSameSender) {
-  //   const index = chats.findIndex(
-  //     (chat) => chat.date.substring(0, 10) === date.substring(0, 10)
-  //     && (chat.sender_id === senderId || chat.sender_id === receiverId),
-  //   );
-
-  //   if (index !== -1) {
-  //     const messageId = nanoid(16);
-  //     const createdAt = new Date().toISOString();
-  //     const newMessage = {
-  //       user_id: senderUserId, message_id: messageId, text, created_at: createdAt,
-  //     };
-
-  //     chats[index].messages.push(newMessage);
-
-  //     const response = h.response({
-  //       status: 'success',
-  //       message: 'pesan berhasil dikirimkan',
-  //     });
-
-  //     response.code(201);
-  //     return response;
-  //   }
-  // }
-
-  // const response = h.response({
-  //   status: 'fail',
-  //   message: 'pesan gagal dikirimkan',
-  // });
-
-  // response.code(500);
-  // return response;
+  return response;
 };
 
-const deleteChatByIdHandler = (request, h) => {
-  // const { chatId } = request.params;
+const deleteChatByIdHandler = async (request, h) => {
+  const { db, ObjectID } = request.mongo;
+  const { chatId } = request.params;
 
+  const status = await db.collection('chats').deleteOne({ chat_id: new ObjectID(chatId) });
+  console.log(status);
+
+  const response = h.response({
+    status: 'success',
+    message: 'Pesan berhasil dihapus',
+  });
+
+  return response;
 };
 
 // fix get all chat by sender id
